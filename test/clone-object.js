40088c8e585639a2eb087156e76f3393e492f02c
rajveerkaur <miths09@gmail.com>
2014-11-25 12:24:48 -0600

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -7,3 +7,32 @@ describe('clone object', function () {
     expect(obj).not.toBe(expected);
   });
 });
+
+function describe(obj) {
+   
+ var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']}
+ 
+  if (null == obj || "object" != typeof obj) return obj;
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+   
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Not supported.");
+}
